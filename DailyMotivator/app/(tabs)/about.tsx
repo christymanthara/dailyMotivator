e87667e26f60d9d0def29eece11fbc0c61f51a7f
thechristyjo <metalliccj@gmail.com>
2024-11-26 13:35:20 +0100

    base project created

---
+++ DailyMotivator/app/(tabs)/about.tsx
@@ -0,0 +1,24 @@
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function AboutScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>About screen</Text>
+      <p style={styles.text}>
+        Created by Christy. All rights reserved
+      </p>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+});
